db38fc08fbc3ef67c1fa387f88532100f5e82cf2
ducvg <117010800+ducvg@users.noreply.github.com>
2023-10-13 16:13:36 +0700

    Add files via upload

---
+++ Tuần 5/demo/src/main/java/com/example/demo/Controller/EditController.java
@@ -0,0 +1,34 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EditController {
+    @Autowired private UserServiceImpl userServiceImpl;
+
+    @GetMapping("/edit/{id}")
+    public String showEdit(@PathVariable("id") Long id, Model model){
+        User u = userServiceImpl.getUserById(id);
+        model.addAttribute("user",u);
+        return "edit";
+    }
+
+    @PostMapping("/edit/update")
+    public String updateUser(User user){
+        userServiceImpl.updateUser(user);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String removeUser(@PathVariable("id") Long id){
+        userServiceImpl.removeUserById(id);
+        return "redirect:/home";
+    }
+}
